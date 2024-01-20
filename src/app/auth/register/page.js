f2296a3f415404f25a747def2c7bf23bb4194c62
leezhengjing <zhengjing.lee86@gmail.com>
2024-01-20 19:10:47 +0800

    Initialize register page

---
+++ src/app/auth/register/page.js
@@ -0,0 +1,7 @@
+export default function registerPage() {
+    return (
+        <main className="flex flex-col items-center justify-between p-24">
+            <h1> Register Page</h1>
+        </main>
+    );
+}
