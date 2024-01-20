e654a14a92fe7c15414df9e340f61cb6c0906d2c
Ryo <seah_ryo@yahoo.com.sg>
2024-01-21 01:13:46 +0800

    Attempt to retrieve data

--- src/app/page.js
+++ src/app/page.js
@@ -1,31 +1,69 @@
-import Image from "next/image";
-import Logout from "./components/logout";
+"use client";
+
 import Link from "next/link";
-import db from "@/db";
-import { use } from "react";
 
-const getUser = async () => {
-  const cookieStore = cookies();
+import { useState, useEffect } from "react";
 
-  const result = await db.getUser(cookieStore);
+// const getUser = async () => {
+//   const cookieStore = cookies();
 
-  return result;
-}
+//   const result = await db.getUser(cookieStore);
 
-async function getTests() {
-  const user = use(getUser());
-  const tests = await db.client.collection("quizzes").getList(1, 50, {
-    filter: `user == ${user.id}`,
-  });
-  console.log(tests)
-  return tests;
-}
+//   return result;
+// }
+
+// async function getTests() {
+//   const user = use(getUser());
+//   const tests = await db.client.collection("quizzes").getList(1, 50, {
+//     filter: `user == ${user.id}`,
+//   });
+//   console.log(tests)
+//   return tests;
+// }
 
 export default function Home() {
+  const [tests, setTests] = useState([]);
+  const [page, setPage] = useState(1);
+  const retrieveTests = async () => {
+    try {
+      const response = await fetch("/api/getTests", {
+        method: "GET",
+        headers: {  "Content-Type": "application/json" },
+      });
+      const data = await response.json();
+      console.log(data);
+      setTests(data);
+    } catch (error) {
+      console.error('Error retrieving test:', error);
+  }
+  }
+  retrieveTests();
+  
+  
+    const handleNextPage = () => {
+      setPage((prevPage) => prevPage + 1);
+    };
+  
+    const handlePrevPage = () => {
+      setPage((prevPage) => Math.max(prevPage - 1, 1));
+    };
   return (
     <main className="flex flex-col items-center justify-between p-24">
       <h1> Home Page</h1>
       <Link href="/generate">Create test</Link>
+      <div>
+      <h1>Tests</h1>
+      <ul>
+        {tests.map((test) => (
+          <li key={test.id}>{/* Render your test data here */}</li>
+        ))}
+      </ul>
+      <button onClick={handlePrevPage} disabled={page === 1}>
+        Previous Page
+      </button>
+      <button onClick={handleNextPage}>Next Page</button>
+    </div>
     </main>
+    
   );
 }
